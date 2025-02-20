a69935b248e273eaa538e54e861c726b4e3a45ea
Tub <colorpulse@gmail.com>
2025-02-20 13:49:04 +0100

    revert commits

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef } from "react"
 import SEO from "../components/seo"
 import gsap from "gsap"
-import Project  from "../components/Project.js"
+import Project from "../components/Project.js"
 import { getImages } from "../helpers/techImages"
 import { useStaticQuery, graphql } from "gatsby"
 import SideBarCollapsed from "../components/SideBarCollapsed"
@@ -54,15 +54,12 @@ const Projects = ({ transitionStatus, location }) => {
   }, [])
 
   const scrollToDiv = ref => console.log(ref)
-console.log(data)
+
   return (
     <>
       {" "}
       <SideBarCollapsed currentWindow={location.pathname} />
-      <div
-        style={{ opacity: 0, position: "relative"}}
-        className="projects"
-      >
+      <div style={{ opacity: 0, position: "relative" }} className="projects">
         <SEO title="Projects" />
         <p className="second-title background-video">
           {location.pathname.substring(1).replace(/\/$/, "")}
